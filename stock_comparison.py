3af42f0c96010a1a6ebddccef5f871d841b507b0
pswaldia <pswaldia1@gmail.com>
2018-06-20 12:51:04 +0530

    standalone page, not linked

--- stock_comparison.py
+++ stock_comparison.py
@@ -10,7 +10,7 @@ from app import app
 df=pd.read_csv('tickers.csv')
 # graph_data=web.DataReader(value,)
 
-layout=html.Div(
+app.layout=html.Div(
 [
 
 html.Div([
@@ -22,67 +22,41 @@ html.Div([
        html.H1([html.Span('24'),html.Span('×',style=dict(opacity=0.5)),html.Span('7')]),
        html.H6('Stock Update')
        ],className="two columns gs-header gs-accent-header padded",style=dict(float='right')),
-       ],className="row gs-header gs-text-header"),
+       ],className="row gs-header gs-text-header",style={'paddingLeft':10}),
        html.Br([]),
+       html.Div([
        html.Div([
         html.Label('Select Company I:',style=dict(color='#7F90AC')),
         dcc.Dropdown(
-        id='stock-ticker-input1',
+        id='stock_ticker_input1',
         options=[{'label':s[0],'value':str(s[1])} for s in zip(df.Company,df.Symbol)],
         value='GOOGL',multi=False
-)],style={'width':'1200','float':'left'}),
+)]),
+# ,style={'width':'1200','float':'left'}
 html.Br([]),
 html.Br([]),
 html.Div([
  html.Label('Select Company II:',style=dict(color='#7F90AC')),
  dcc.Dropdown(
- id='stock-ticker-input2',
+ id='stock_ticker_input2',
  options=[{'label':s[0],'value':str(s[1])} for s in zip(df.Company,df.Symbol)],
  value='YHOO',multi=False
-)],style={'width':'1200','float':'left','marginBottom':20,'marginLeft':20,'marginRight':20}),
-
+)],style={'marginBottom':20}),
+],style={'padding':20}),
 html.Br([]),
+html.Div(id='graphing_area',style={'marginTop':150})
+
+])
 
 
 
-# html.Div([
-#     dcc.DatePickerRange(
-#         id='my-date-picker-range',
-#         min_date_allowed=dt(1995, 8, 5),
-#         max_date_allowed=dt(2025, 12, 30),
-#         initial_visible_month=dt(2017, 8, 5),
-#         end_date=dt(2017, 8, 25)
-#     ),
-#     html.Div(id='output-container-date-picker-range')]),
-    html.Div(id='graphing_area',style={'marginTop':150})
-
-],style={'marginLeft':20,'marginRight':20})
-
-
-# @app.callback(
-#     dash.dependencies.Output('output-container-date-picker-range', 'children'),
-#     [dash.dependencies.Input('my-date-picker-range', 'start_date'),
-#      dash.dependencies.Input('my-date-picker-range', 'end_date')])
-# def update_output(start_date, end_date):
-#     string_prefix = 'You have selected: '
-#     if start_date is not None:
-#         start_date = dt.strptime(start_date, '%Y-%m-%d')
-#         start_date_string = start_date.strftime('%B %d, %Y')
-#         string_prefix = string_prefix + 'Start Date: ' + start_date_string + ' | '
-#     if end_date is not None:
-#         end_date = dt.strptime(end_date, '%Y-%m-%d')
-#         end_date_string = end_date.strftime('%B %d, %Y')
-#         string_prefix = string_prefix + 'End Date: ' + end_date_string
-#     if len(string_prefix) == len('You have selected: '):
-#         return 'Select a date to see it displayed here'
-#     else:
-#         return string_prefix
 @app.callback(
 Output(component_id='graphing_area',component_property='children'),
-[Input(component_id='stock-ticker-input1',component_property='value')
-,Input(component_id='stock-ticker-input2',component_property='value')]
+[Input(component_id='stock_ticker_input1',component_property='value'),
+Input(component_id='stock_ticker_input2',component_property='value')]
+
 )
-def update_graph(graph_1,graph_2):
+def update(graph_1,graph_2):
     graph_data1=web.DataReader(graph_1,'morningstar',dt.datetime(2017,5,5),dt.datetime.now())
     graph_data1.reset_index(inplace=True)
     graph_data2=web.DataReader(graph_2,'morningstar',dt.datetime(2017,5,5),dt.datetime.now())
@@ -91,9 +65,7 @@ def update_graph(graph_1,graph_2):
     graph=dcc.Graph(id='graph',figure={'data':[{'x':graph_data1.Date,'y':graph_data1.Close,'name':graph_1},
                                     {'x':graph_data2.Date,'y':graph_data2.Close,'name':graph_2}
                                     ],
-                            'layout':{'title':'Comparison of two companies on the basis of closing Prices:'},
-                            'xaxis':{'title':'Dates'},
-                            'yaxis':{'title':'Closing Prices'}
+                            'layout':{'title':'Comparison of two companies on the basis of closing Prices:'}
                             })
     return graph
 
